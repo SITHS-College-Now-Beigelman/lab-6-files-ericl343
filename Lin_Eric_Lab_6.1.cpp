bbd3c297c7d71f963fb4361703967fd7cf091409
ericl343 <ericl343@nycstudents.net>
2024-10-15 16:56:43 -0400

    finally

--- Lin_Eric_Lab_6.1.cpp
+++ Lin_Eric_Lab_6.1.cpp
@@ -23,17 +23,27 @@ int v2;
 char x;
 string hi;
 
+//Adding value 1 and 2 together
 inFile >> v1 >> v2;
 outFile << "Value of combined:" << v1+v2 <<endl;
 
+//The next letter from my char
 inFile >> x;
 outFile <<"char:"<<char(x+1) << endl;
 
+//String
 inFile>>hi;
 outFile<<hi;
 
+//The files are used and should be closed
 outFile.close();
 inFile.close();
 
 return 0;
 }
+
+/*
+Value of combined:79
+char:F
+Sup!
+*/
